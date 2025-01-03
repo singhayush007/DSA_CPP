bc3a1aeee81e80a7fad03c8c45dc53dad4ad674b
Ayush <ayushsingh24958@gmail.com>
2025-01-03 12:00:00 +0530

    Added Hard_pattern.cpp and Hard_pattern

---
+++ Hard_pattern.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+/*int main() {
+    int row, col;
+    int n;
+    cout << "Input the number: ";
+    cin >> n;
+
+    for (row = 1; row <= n; row = row + 1) {
+        // Space print
+        for (col = 1; col <= n - row; col = col + 1) {
+            cout << "  ";
+        }
+
+        // Star print
+        for (col = 1; col <= row; col = col + 1) {
+            cout << "* ";
+        }
+
+        // Move to the next line after each row
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+int main() {
+    int row, col;
+    int n;
+    cout << "Input the number: ";
+    cin >> n;
+
+    for (row = 1; row <= n; row = row + 1) {
+        // Space print
+        for (col = 1; col <= n - row; col = col + 1) {
+            cout << "  ";
+        }
+
+        // Number print
+        for (col = 1; col <= row; col = col + 1) {
+            cout << row << " ";
+        }
+
+        // Move to the next line after each row
+        cout << endl;
+    }
+
+    return 0;
+}*/
+
+
+/* int main() {
+    int row, col;
+    int n;
+    cout << "Input the number: ";
+    cin >> n;
+
+    for (row = 1; row <= n; row = row + 1) {
+        // Space print
+        for (col = 1; col <= n - row; col = col + 1) {
+            cout << "  ";
+        }
+
+        // Number print
+        for (col = 1; col <= row; col = col + 1) {
+            cout << col << " ";
+        }
+
+        // Move to the next line after each row
+        cout << endl;
+    }
+
+    return 0;
+} */
+
+  /* int main() {
+    int row , col;
+    int n;
+    cout << "Input the number: ";
+    cin >> n;
+
+    for ( row = 1; row <= n ; row = row + 1)
+    {
+        // Space print
+        for ( col = 1; col <= n-row; col = col + 1)
+        cout <<"  ";
+
+        // CHar print
+        for ( col = 1; col <= row; col = col +1)
+        {
+            char name = 'A' + col - 1;
+            cout<<name<<" ";
+        }
+        cout<<endl;
+    }
+  }*/
+
