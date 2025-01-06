0f1eeb23522054c24508db85e0749bd99cf7b62e
Ayush <ayushsingh24958@gmail.com>
2025-01-06 15:37:31 +0530

    Added While.cpp file

---
+++ While.cpp
@@ -0,0 +1,170 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /* Binary Operator 
+       cout<<13.7/2;
+
+     Post Increment
+     int a = 10;
+     cout<<a++;
+     cout<<endl<<a;
+    
+     int a = 10;
+     int b = a++;
+     cout<<b<<" "<<a; 
+
+     Pre Increment
+     int a = 10; 
+     int b  = ++a;
+     cout<<b<<" "<<a; 
+
+     // Post Decrement
+     int a = 10;
+     int b = a--;
+     cout<<b<<"  "<<a; 
+
+     // Pre Decrement
+
+     int a = 10;
+     int b = --a;
+     cout<<b<<"  "<<a; */
+   
+
+   // Comparison Operator -->  == 
+   /* int a , b;
+   cout<<"Enter the value of a: ";
+   cin>>a;
+   cout<<"Enter the value of b: ";
+   cin>>b; */
+
+   /* if (a ==b)
+
+   cout<<"Yes";
+   else
+   cout<<"No"; */
+
+   // Greator    Operator -->  >
+
+   /*if (a > b)
+   cout<<"Yes";
+   else
+   cout<<"No"; */
+
+   // Less Than Operator -->   <
+
+   /* if (a < b)
+   cout<<"Yes";
+   else
+   cout<<"No"; */
+
+   // Greater Equal Opertor --> >=
+   
+   /* if (a >= b)
+   cout<<"Yes";
+   else
+   cout<<"No */
+
+   // Less Equal Operator --> <=
+
+   /* if (a <= b)
+   
+   cout<<"Yes";
+   else
+   cout<<"No */
+
+   // Not Equal Operator --> != 
+
+   /* if (a != b)
+   
+   cout<<"Yes";
+   else
+   cout<<"No */
+
+  // Logical Operator --> { && AND , || OR , ! NOT }
+
+  //  && AND Operator 
+  /* int a , b, c;
+  cin>>a>>b>>c;
+
+  if(a>b&&a>c)
+  cout<<"Yes";
+  else
+  cout<<"No"; */
+
+  // || OR Operator
+
+  /* char name = 'b';
+  if( name== 'a' || name == 'e' || name == 'i' || name == 'o' || name == 'u')
+  cout<<'Vowel';
+  else 
+  cout<<"Consonant"; */
+
+  // ! NOT Operator
+ /* cout<<!128<<endl; */
+
+  
+  
+  // Precedence Rule in Operator ---> { > , < , >= , <= } --> {== , !=} --> {&&} --> {||}
+
+
+  
+ /*   Bitwise Operator 
+
+ & Operator
+
+int ans = 2&3;
+cout<<ans<<endl;
+
+ 0R Operator
+
+int ans = 2|3;
+cout<<ans<<endl;
+
+ ^ Operator
+
+int ans = 2^3;
+cout<<"My answer is: "<<ans<<endl;
+
+
+ Left shift Operator
+
+int ans = 2<<3;
+cout<<"My anwer is :" <<ans<<endl;
+
+ Right Shift Operator
+
+int ans = 18>>3;
+cout<<"My answer is: "<<ans<<endl;
+
+ Compliment Operator
+
+int ans = ~10;
+cout<<"My answer is: "<<ans<<endl; */
+
+/* Assignment operator 
+
+int a = 10;
+a+=15;  // a = a + 15
+cout<<a;
+
+int a = 10;
+a*=15; // a = a * 15
+cout<<a; 
+
+if ( 8 > 5 > 3 < 2 * 4)
+cout<<"Yes";
+else
+cout<<"No"; */
+
+
+
+
+
+
+
+
+
+     
+}
