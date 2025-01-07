72b69c4ec4304729aa1ef33a2e62143d224ee44e
Ayush <ayushsingh24958@gmail.com>
2025-01-07 15:17:47 +0530

    Added While_Do_While_loop.cpp file

---
+++ While_Do_while_loop.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    int i = 1; // initialize
+
+     while (i <= 10) // Break
+
+    {
+        cout << i * n << endl;
+        i++; // Update
+    }
+
+    do
+    {
+        cout << i << endl;
+        i++;
+    } while (i <= n); */
+
+    // Break continue
+
+    /* for (int i = 1; i<= 10; i++)
+    {
+        if( i ==5)
+        break;
+
+        cout<<i<<"  ";
+    }
+
+    for( int i = 1; i <=10; i++)
+    {
+        if( i ==5)
+        continue;
+
+        cout<<i<<"  ";
+    } */
+
+    // Switch Statement
+
+    /* int i;
+    cout << "Enter the number: ";
+    cin >> i;
+
+    switch (i)
+    {
+        case 1:
+        cout<<"Rohit";
+        break;
+
+        case 2:
+        cout<<"Mohit";
+        break;
+
+        default:
+        cout<<"Sohit"; */
+
+
+        /* int n = 10;
+        int o = 20;
+
+        for(;i<=30; i++)
+        {
+            cout<<i<<"  "; 
+        } 
+
+
+    } */
+
+} 
