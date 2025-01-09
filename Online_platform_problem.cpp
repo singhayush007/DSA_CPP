ee4ca28c3eb1ac8a15290ccd299cb816098bd002
Ayush <ayushsingh24958@gmail.com>
2025-01-09 15:38:18 +0530

    Added Online_platform_problem.cpp file

---
+++ Online_platform_problem.cpp
@@ -0,0 +1,17 @@
+// For an input year N , find whether the year is a leap year or not .
+
+class solution
+{
+public:
+    int isLeap(int N)
+    {
+        // CODE HERE
+        if (N % 400 == 0)
+            return 1;
+
+        else if (N % 4 == 0 && N % 100 != 0)
+            return 1;
+        else
+            return 0;
+    }
+};
