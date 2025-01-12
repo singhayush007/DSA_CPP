339f4c81bbd0832bd9879c11d240a7c191270094
Ayush <ayushsingh24958@gmail.com>
2025-01-12 12:30:36 +0530

    Add Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+/* int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
+
+// Size of an Array
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    cout << sizeof(arr) << " ";
+
+    return 0;
+}
+
+int main()
+{
+    int arr[5] = {2, 3, 1, 7, 8};
+    int a;
+    cout << sizeof(a) << endl;
+    return 0;
+}
+
+int main()
+{
+    int arr[5] = {2, 3, 1, 7, 8};
+    cout << sizeof(arr) / sizeof(arr[0]) << endl;
+    return 0;
+}
+
+int main()
+{
+    int arr[5] = {2, 3, 1, 7, 8};
+    int ans = INT_MAX;
+
+    // MIN Value
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] < ans)
+            ans = arr[i];
+    }
+
+    cout << ans << endl;
+}
+
+// MAX Value
+
+ans = INT_MIN;
+
+for (int i = 0; i < 5; i++)
+{
+    if (arr[i] > ans)
+        ans = arrr[i];
+
+        cout<ans<endl;
+}
+reutrn 0;
+}
+ */
